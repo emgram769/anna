dd9d54941ff219e8266e4776ec11925d84e00daa
OP <ec2-user@ip-172-31-35-138.ec2.internal>
2014-12-21 22:45:30 -0500

    initial commit

---
+++ anna.py
@@ -0,0 +1,125 @@
+from time import sleep
+from livechanapi import *
+import wikipedia
+from pybing import Bing
+import duckduckgo
+import re
+import sys
+import wolframalpha
+import random
+from datetime import datetime
+import config
+
+if (len(sys.argv) < 2):
+    print "Usage: python anna.py [channel]"
+    exit()
+channel = sys.argv[1]
+bing = Bing(config.bingAPI)
+wolfram = wolframalpha.Client(config.wolframAPI)
+
+# globals
+curr_bing = None
+users = {}
+
+def process_chat(*args):
+    img = "anna" + str(random.randint(6,8)) + ".png"
+    try:
+        ident = args[0]["identifier"]
+        global users
+        if ident in users:
+            interval = (datetime.now() - users[ident]).total_seconds()
+            if interval < 7:
+                return
+        message = args[0]["body"]
+        name = args[0]["name"]
+        count = str(args[0]["count"])
+        if "trip" in args[0]:
+            trip = args[0]["trip"]
+        else:
+            trip = ""
+        if trip == "!!.w5vzYxkv6":
+            return
+        # default message
+        out_message = ""
+
+        # helpful
+        t = re.compile('[wW]hat (is|are) (.+)\?').match(message)
+        if (t):
+            try:
+                res = wolfram.query(t.group(2))
+                out_message = next(res.results).text
+                #out_message = wikipedia.summary(t.group(1), sentences=1)
+            except Exception as e:
+                print res.__dict__
+                print out_message
+                out_message = ""
+                print "wolfram error",e
+
+        # wolfram
+        t = re.compile('\.wa (.+)').match(message)
+        if (t):
+            try:
+                res = wolfram.query(t.group(1))
+                out_message = next(res.results).text
+            except Exception as e:
+                out_message = "That query was ambiguous, Onii-chan"
+                img = "anna" + str(random.randint(1,5)) + ".png"
+                print e
+         
+        # wiki
+        t = re.compile('\.wiki(pedia)? (.+)').match(message)
+        if (t):
+            try:
+                out_message = wikipedia.summary(t.group(2), sentences=3)
+            except Exception as e:
+                out_message = "That query was ambiguous, Onii-chan"
+                img = "anna" + str(random.randint(1,5)) + ".png"
+                print e
+     
+        # google
+        t = re.compile('\.google( (.+))?').match(message)
+        if (t):
+            try:
+                r = duckduckgo.query(t.group(2))
+                for i in xrange(len(r.related) if len(r.related) < 4 else 3):
+                    result = r.related[i]
+                    out_message += '\n'+ result.text + '\n'
+                    out_message += '[i]' + result.url + ' [/i]\n'
+            except Exception as e:
+                out_message = "That query was ambiguous, Onii-chan"
+                img = "anna" + str(random.randint(1,5)) + ".png"
+                print e
+
+        # random
+        t = re.compile('\.random( (.+))?').match(message)
+        if (t):
+            try:
+                if t.group(1) and t.group(2).isdigit():
+                    out_message += str(random.randint(0, int(t.group(2))))
+                else:
+                    out_message += str(random.randint(0, 100))
+                    if int(out_message)%10 == int(out_message)/10:
+                        out_message += " (you got doubles :3)"
+            except Exception as e:
+                out_message = "That was ambiguous, Onii-chan"
+                img = "anna" + str(random.randint(1,5)) + ".png"
+                print e
+
+        #out_message = (">>"+str(args[0]["count"])+
+        #"\n\ryou said my name, Onii-chan?")
+
+        if out_message != "":
+            users[ident] = datetime.now()
+            if (count):
+                out_message = ">>"+count+"\n"+out_message#.encode('ascii', 'ignore')
+            post_chat(out_message, channel,
+                    name="anna", trip=config.annaTrip,
+                    convo=args[0]["convo"], file=img)
+    except Exception as e:
+        print e
+
+login(callback=process_chat)
+join_chat(channel)
+
+while 1:
+    sleep(10)
---
+++ livechanapi.py
@@ -0,0 +1,137 @@
+import poster
+import urllib2
+import cookielib
+import re
+from socketIO_client import SocketIO
+import json
+import thread
+import config
+
+cookies = cookielib.LWPCookieJar()
+
+handlers = [
+    urllib2.HTTPHandler(),
+    urllib2.HTTPSHandler(),
+    urllib2.HTTPCookieProcessor(cookies)
+    ]
+#opener = urllib2.build_opener(*handlers)
+# Register the streaming http handlers with urllib2
+opener = poster.streaminghttp.register_openers()
+for handler in handlers:
+    opener.add_handler(handler)
+
+curr_chat_room = None
+socketIO = None
+mesg_queue = []
+def fetch(uri):
+    req = urllib2.Request(uri)
+    return opener.open(req)
+
+def post(uri, params):
+    datagen, headers = poster.encode.multipart_encode(params)
+    req = urllib2.Request(uri, datagen, headers)
+    return opener.open(req)
+
+def get_password():
+    for cookie in cookies:
+        if cookie.name == "password_livechan":
+            cookie.value = config.nolimitCookie
+            return cookie.value
+    return ""
+
+def post_chat(body, chat, name="Anonymous", convo="General", trip="", file="" ):
+    post_params = []
+
+    name = name if trip == "" else name+"##"+trip
+    post_params.append(poster.encode.MultipartParam("name", name))
+    post_params.append(poster.encode.MultipartParam("trip", trip))
+    post_params.append(poster.encode.MultipartParam("body", body))
+    post_params.append(poster.encode.MultipartParam("convo", convo))
+    post_params.append(poster.encode.MultipartParam("chat", chat))
+
+    if not file == "":
+        post_params.append(poster.encode.MultipartParam.from_file("image", file))
+
+    datagen, headers = poster.encode.multipart_encode(post_params)
+    uri = 'https://livechan.net/chat/'+chat
+    req = urllib2.Request(uri, datagen, headers)
+    return opener.open(req)
+    #return post('https://livechan.net/chat/'+chat, post_params)
+
+
+def join_chat(chat_room):
+    global curr_chat_room
+    global socketIO
+    if (curr_chat_room != None):
+        socketIO.emit('unsubscribe', curr_chat_room);
+    socketIO.emit('subscribe', chat_room);
+
+def display_chat(chat_obj):
+    print chat_obj["name"]
+    print chat_obj["body"]
+    print
+
+def get_data(chat):
+    data_response = fetch('https://livechan.net/data/'+chat)
+    json_data = json.loads(data_response.read())
+    for i in json_data[::-1]:
+        display_chat(i)
+
+def main_chat(chat_room):
+    chat_body = raw_input("> ")
+    if (chat_body == "/quit"):
+        return True # break
+    mainresp = post_chat(chat_body, chat_room)
+    return False
+    #print mainresp.read()
+
+def on_chat(*args):
+    if (args[0]["name"] == "IRCBot"):
+        return
+    msg = args[0]["name"]+"~ "
+    if ("image" in args[0]):
+        filename = "https://livechan.net/tmp/uploads/"+re.compile('[\w\-\.]*$').search(args[0]["image"]).group(0)
+        msg += "file: "+filename+" "
+    msg += (" ".join(args[0]["body"].splitlines()))
+    livechanBot.sendMsg(channel, msg)
+
+def on_user_count(*args):
+    print args[0], "users online"
+    print
+
+def on_request_location(*args):
+    global chat_room
+    socketIO.emit('subscribe', curr_chat_room);
+
+def login(callback=on_chat):
+    #image_response = fetch('https://livechan.net/captcha.jpg')
+    #image_data = image_response.read()
+
+    #with open('captcha.jpg', 'w') as f:
+    #    f.write(image_data)
+    #os.system("open captcha.jpg")
+
+    #digits = int(raw_input("enter the captcha: "))
+    #post_params = {}
+    #post_params["digits"] = digits
+    #login_response = post('https://livechan.net/login', post_params)
+    #login_html = login_response.read()
+
+    cookie = cookielib.Cookie(version=0, name='password_livechan', value='jRRu12!', port=None, port_specified=False, domain='livechan.net',
+            domain_specified=False, domain_initial_dot=False,
+            path='/', path_specified=True, secure=False, expires=None,
+            discard=True, comment=None, comment_url=None, rest={'HttpOnly': None}, rfc2109=False)
+    cookies.set_cookie(cookie)
+    livechan_pass = get_password()
+    if livechan_pass == "":
+        print "wrong password"
+        login()
+
+    global socketIO
+    socketIO = SocketIO('https://livechan.net',
+        cookies={'password_livechan': livechan_pass})
+    socketIO.on('chat', callback)
+    socketIO.on('request_location', on_request_location)
+    
+    thread.start_new_thread ( socketIO.wait, () )
+ 
